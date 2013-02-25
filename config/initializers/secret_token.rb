e20d189af758df03970d38cffb5ae3b67ca91d0d
kstefanie <kstefanie@gmail.com>
2013-02-24 22:44:14 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pipi::Application.config.secret_token = '39960cfb0503d2855d585400148b698a9c4938af2e6547da39ebd6ed5a30b63d9cbfa749bace6c2391e547ea5976d0d18acb3645f9da83d1005ab68490729a70'
