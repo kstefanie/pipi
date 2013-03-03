f56deba321858e9698877e36396dbf726781629f
kstefanie <kstefanie@gmail.com>
2013-03-03 00:05:26 -0800

    finish

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -17,8 +17,5 @@ module SessionsHelper
     @current_user ||= User.find_by_remember_token(cookies[:remember_token])
   end
 
-  def sign_out
-    self.current_user = nil
-    cookies.delete(:remember_token)
-  end
+  
 end
